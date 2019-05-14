f526ec77a59efb6ecc44fbab86c2bd4038807be8
yunus <ynsture@gmail.com>
2019-05-14 20:51:09 +0000

    Middleware and Logout Added

---
+++ README.md
@@ -0,0 +1,6 @@
+#Skılls ThatI Learned in This App:
+
+
+#Authentication's Logic
+#Passport.js
+#Middleware
--- app.js
+++ app.js
@@ -28,7 +28,8 @@ app.get("/",(req,res)=>{
     res.render("home");
 });
 
-app.get("/secret",(req,res)=>{
+app.get("/secret",isLoggedIn,(req,res)=>{
+    
      res.render("secret")
 });
 
@@ -64,7 +65,23 @@ app.post("/login",passport.authenticate("local",{
 }),(req,res)=>{
     
     
-})
+});
+/// log out route
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/")
+});
+
+
+/*================
+Middleware
+================*/
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+   res.redirect("/login") 
+}
 
      /*===========
       SERVER LİSTEN
